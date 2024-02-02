b36849cc3f2656bff4afd4c8c74a8e96e8373f2d
saridaemayank <jadeslayer1026@gmail.com>
2024-02-02 15:34:24 +0530

    First commit

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:study_sensei/themes/theme_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
